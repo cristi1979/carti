0361ad8fdfbae52fca3c8afd0f6d8d751fcb86dc
Cristian Falcas <cristi.falcas@gmail.com>
2012-02-08 20:27:55 +0200

    dsg

--- transform.pl
+++ transform.pl
@@ -42,7 +42,7 @@ use Carti::Common;
 use Carti::WikiTxtClean;
 
 my $DataQueue = Thread::Queue->new();
-my $max_html_parse_threads = 6;
+my $max_html_parse_threads = 1;
 
 my $script_dir = (fileparse(abs_path($0), qr/\.[^.]*/))[1]."";
 my $extra_tools_dir = "$script_dir/tools";
